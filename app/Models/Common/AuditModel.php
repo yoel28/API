ca552d3af1d45060dc80aa5031cc804061fff712
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-17 15:03:33 -0400

    update audit

--- app/Models/Common/AuditModel.php
+++ app/Models/Common/AuditModel.php
@@ -4,9 +4,16 @@ namespace Api\Models\Common;
 
 use Illuminate\Database\Eloquent\Model;
 
-
 class AuditModel extends Model
 {
-    protected $fillable = ["ip", "user_agent"];
-    protected $hidden = ["ip", "user_agent"];
+    protected $fillable = ['ip', 'user_agent','created_at','updated_at','deleted_at'];
+    protected $hidden = ['ip', 'user_agent','created_at','updated_at','deleted_at'];
+    protected $guarded = [];
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function addFillabel(array $fillabel){
+        $this->fillable(array_merge($this->fillable, $fillabel));
+    }
+
+
 }
