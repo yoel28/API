527fdb4957dca6dd681c597cde1fff60c81f27a9
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-17 18:58:16 -0400

    update model

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,12 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(Api\UserModel::class, function (Faker\Generator $faker) {
+$factory->define(Api\Models\Access\UserModel::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
     ];
 });
