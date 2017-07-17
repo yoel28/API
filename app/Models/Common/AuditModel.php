8ab057595f287772ddaacd1cbff0949e19f97c25
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-17 15:12:31 -0400

    add softDeletes

--- app/Models/Common/AuditModel.php
+++ app/Models/Common/AuditModel.php
@@ -3,9 +3,12 @@
 namespace Api\Models\Common;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AuditModel extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['ip', 'user_agent','created_at','updated_at','deleted_at'];
     protected $hidden = ['ip', 'user_agent','created_at','updated_at','deleted_at'];
     protected $guarded = [];
