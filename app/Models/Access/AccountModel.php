55f48c520c4130e8549fa5fc4447500221307f71
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-17 07:29:53 -0400

    add model

---
+++ app/Models/Access/AccountModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Api\Models\Access;
+
+use Api\Models\Common\BaseModel;
+
+class AccountModel extends BaseModel
+{
+
+    public function add_hidden(){
+        $this->addHidden(
+            [
+                'account_id'
+            ]
+        );
+    }
+
+    public function add_fillable(){
+        $this->addFillabel(
+            [
+                "address",
+                "contact",
+                "email",
+                "favicon",
+                "hostname",
+                "icon",
+                "max_user_count",
+                "message",
+                "name",
+                "phone",
+                "preferences",
+                "url"
+            ]
+        );
+    }
+
+}
