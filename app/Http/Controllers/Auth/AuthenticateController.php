9e8ff65980f9b9176cda3c084f80db54e8f67eda
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-18 17:11:47 -0400

    add function getAuthenticatedUser

--- app/Http/Controllers/Auth/AuthenticateController.php
+++ app/Http/Controllers/Auth/AuthenticateController.php
@@ -6,10 +6,12 @@ use Api\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class AuthenticateController extends Controller
 {
-    public function store(Request $request)
+    public function Authenticate(Request $request)
     {
         // grab credentials from the request
         $credentials = $request->only('email', 'password');
@@ -26,6 +28,31 @@ class AuthenticateController extends Controller
 
         // all good so return the token
         return response()->json(compact('token'));
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+
+        } catch (TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+
+        } catch (TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
+        } catch (JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+
+        }
+        // the token is valid and we have found the user via the sub claim
+        return response()->json(compact('user'));
     }
 
 }
