f3192cc9f0c6af2d07c3a1e6f260f20725af3fbb
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-17 21:01:46 -0400

    add middleware jwt

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,6 +50,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'jwt' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
