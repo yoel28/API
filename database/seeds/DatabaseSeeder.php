4558c514398e3f32a527a6441f5305515944cda1
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-18 17:12:48 -0400

    seeder bug model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard();
+        factory(\Api\Models\Access\UserModel::class, 10)->create();
+        Model::reguard();
+
         // $this->call(UsersTableSeeder::class);
     }
 }
