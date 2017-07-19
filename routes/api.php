eebbc953dcd287cb15401ceaf7148893c6b7a59b
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-18 21:28:22 -0400

    update router

--- routes/api.php
+++ routes/api.php
@@ -13,24 +13,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 function methodsBase():array {
-    return ['index', 'search', 'store', 'update', 'destroy', 'show'];
+    return ['index', 'store', 'update', 'destroy', 'show'];
 }
 
 Route::pattern('id','\d+');
 
-Route::post('login','Auth\AuthenticateController@Authenticate');
-Route::post('validate','Auth\AuthenticateController@getAuthenticatedUser');
 
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login','Auth\AuthenticateController@Authenticate');
+    Route::post('validate','Auth\AuthenticateController@getAuthenticatedUser');
+    Route::post('refresh', ['middleware' => ['jwt.refresh'], function() {
+        $user = JWTAuth::parseToken()->toUser();
+        return Response::json(compact('user'));
+    }]);
+});
 
-Route::group(['middleware' => ['jwt']], function () {
-    Route::resource('access/accounts','Access\AccountController',
-        ['only' => methodsBase()]
-    );
-    Route::resource('access/users','Access\UserController',
-        ['only' => methodsBase()]
-    );
+Route::group(['middleware' => ['jwt.auth']], function () {
 
-    Route::resource('business/rules','Business\RuleController',
-        ['only' => methodsBase()]
-    );
+    Route::group(['prefix' => 'access'], function () {
+        Route::resource('accounts','Access\AccountController',
+            ['only' => methodsBase()]
+        );
+        Route::resource('users','Access\UserController',
+            ['only' => methodsBase()]
+        );
+
+        Route::group(['prefix' => 'search'], function () {
+            Route::get('accounts/{value?}', 'Access\AccountController@search');
+            Route::get('users/{value?}', 'Access\UserController@search');
+        });
+    });
+
+    Route::group(['prefix' => 'businness'], function () {
+        Route::resource('rules','Business\RuleController',
+            ['only' => methodsBase()]
+        );
+        Route::group(['prefix' => 'search'], function () {
+            Route::get('rules/{value?}', 'Business\RuleController@search');
+        });
+
+    });
 });
