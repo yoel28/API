179728e1393e0ad5e44462fb07a84bd2584f2616
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-20 16:44:52 -0400

    update model find account only code & name

--- app/Models/Business/RuleModel.php
+++ app/Models/Business/RuleModel.php
@@ -2,23 +2,21 @@
 
 namespace Api\Models\Business;
 
+use Api\Models\Access\AccountModel;
 use Api\Models\Common\BaseModel;
 
 
 class RuleModel extends BaseModel
 {
+    protected $fillable = ['account_id'];
+    protected $hidden = ['rule_id'];
 
-    public function add_hidden(){
-        $this->addHidden(['rule_id']);
+    public function getAccountIdAttribute($id){
+        return AccountModel::find($id,['code','name']);
     }
 
-    public function add_fillabel(){
-        $this->addFillabel(["account_id"]);
-    }
-
-    public function account()
-    {
-        return $this->belongsTo('Api\Models\Access\AccountModel');
+    public function account(){
+        return $this->belongsTo(\Api\Models\Access\AccountModel::class,'account_id','account_id');
     }
 
 }
