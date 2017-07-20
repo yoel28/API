d33f564abb492f88e0e212c8fc793c42b3b8c864
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-20 16:31:01 -0400

    remove fillabel add hidden audit

--- app/Models/Common/AuditModel.php
+++ app/Models/Common/AuditModel.php
@@ -9,10 +9,20 @@ class AuditModel extends Model
 {
     use SoftDeletes;
 
-    protected $fillable = ['ip', 'user_agent','created_at','updated_at','deleted_at'];
-    protected $hidden = ['ip', 'user_agent','created_at','updated_at','deleted_at'];
-    protected $guarded = [];
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function __construct(array $attributes = []){
+        $this->_initModel();
+        parent::__construct($attributes);
+    }
+
+    private function _initModel(){
+        $this->_addHiddenAudit();
+    }
+
+    protected function _addHiddenAudit(){//TODO: Ocultar dependiendo de los permisos
+        $this->addHidden(['ip', 'user_agent','created_at','updated_at','deleted_at']);
+    }
 
     public function addFillabel(array $fillabel){
         $this->fillable(array_merge($this->fillable, $fillabel));
