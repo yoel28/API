8a02f5d9da4092452f07d09f8cb77beebb90fd59
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-20 16:34:08 -0400

    remove abstract class protected prefix

--- app/Models/Common/BaseModel.php
+++ app/Models/Common/BaseModel.php
@@ -2,39 +2,32 @@
 
 namespace Api\Models\Common;
 
-
-Abstract class BaseModel extends AuditModel
+class BaseModel extends AuditModel
 {
-    private $_prefix = 'Model';
+    protected $_prefix = 'Model';
 
     public function __construct(array $attributes = []){
         $this->_initModel();
         parent::__construct($attributes);
     }
 
-    abstract public function add_hidden();
-    abstract public function add_fillabel();
-
     private function _initModel(){
 
         $this->_setTable();
         $this->_setPK();
 
-        $this->_addHidden();
-        $this->add_hidden();
-
+        $this->_addHiddenBase();
         $this->_addFillabel();
-        $this->add_fillabel();
 
     }
 
-    private function _addHidden(){
-        $this->addHidden(["enabled"]);
+    protected function _addHiddenBase(){
+        $this->addHidden(["enabled"]); //TODO: Depende de los permisos que disponga el usuario
     }
 
     private function _addFillabel(){
         $this->addFillabel(
-            ["deleted", "detail", "editable","code","enabled","images","title","visible"]
+            ["detail", "editable","code","enabled","images","title","visible"]
         );
     }
 
