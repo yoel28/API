3cec8f0b25bf01e074ca666bee8d331b3bce923f
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-20 20:57:13 -0400

    extends BaseRequest

--- app/Http/Requests/Access/AccountRequest.php
+++ app/Http/Requests/Access/AccountRequest.php
@@ -2,40 +2,29 @@
 
 namespace Api\Http\Requests\Access;
 
-use Illuminate\Foundation\Http\FormRequest;
+use Api\Http\Requests\Utils\BaseRequest;
 
-class AccountRequest extends FormRequest
+class AccountRequest extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
-        return [
-            "address" => "required|max:150",
-            "contact" => "max:100",
-            "email" => "required|max:100|unique:account|email",
-            "favicon" => "mimes:jpeg,bmp,png",
-            "hostname" => "required|max:150",
-            "icon" => "mimes:jpeg,bmp,png",
-            "max_user_count" => "required|numeric|min:1",
-            "message" => "",
-            "name" => "required|max:100",
-            "phone" => "max:20",
-            "url" => "max:150",
-            "preferences" => ""
-        ];
+        return array_merge(
+            [
+                "address" => "required|max:150",
+                "contact" => "max:100",
+                "email" => "required|max:100|unique:account|email",
+                "favicon" => "mimes:jpeg,bmp,png",
+                "hostname" => "required|max:150",
+                "icon" => "mimes:jpeg,bmp,png",
+                "max_user_count" => "required|numeric|min:1",
+                "message" => "",
+                "name" => "required|max:100",
+                "phone" => "max:20",
+                "url" => "max:150",
+                "preferences" => ""
+            ],
+            $this->getRules()
+        );
     }
+
 }
