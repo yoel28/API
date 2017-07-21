30e287e9f6b5e716301d1c6aac70c92ad587d944
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-20 20:12:22 -0400

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
-        factory(\Api\Models\Access\UserModel::class, 10)->create();
+//        factory(\Api\Models\Access\UserModel::class, 10)->create();
+        factory(\Api\Models\Access\AccountModel::class, 10)->create();
+        factory(\Api\Models\Business\RuleModel::class, 200)->create();
         Model::reguard();
 
         // $this->call(UsersTableSeeder::class);
