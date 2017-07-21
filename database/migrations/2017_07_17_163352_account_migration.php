a47e4df023e92086c9348c44016e3cd7a1751c02
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-20 20:08:11 -0400

    update migrations use BaseMigration

--- database/migrations/2017_07_17_163352_account_migration.php
+++ database/migrations/2017_07_17_163352_account_migration.php
@@ -2,63 +2,40 @@
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+use Api\Models\Utils\BaseMigration;
 
-class AccountMigration extends Migration
+class AccountMigration extends BaseMigration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('account', function (Blueprint $table) {
+    protected $table = 'account';
 
-            $table->string("address",150);
-            $table->string("contact",100)->nullable(true);
-            $table->string("email",100);
-            $table->text("favicon")->nullable(true);
-            $table->string("hostname",150);
-            $table->text("icon")->nullable(true);
-            $table->integer("max_user_count")->default(1);
-            $table->text("message")->nullable(true);
-            $table->string("name",100);
-            $table->string("phone",20)->nullable(true);
-            $table->string("url",150)->nullable(true);
-            $table->json("preferences")->nullable(true);
-        });
+    public function up(){
+        Schema::create($this->table, function (Blueprint $table) {
 
-        Schema::table('account', function (Blueprint $table) {
             $table->increments('account_id');
 
-            $table->text("detail")->nullable(true);
-            $table->boolean("editable")->default(true);
-            $table->string("code",100)->unique();
-            $table->boolean("enabled")->default(true);
-            $table->text("images")->nullable(true);
-            $table->string("title",100);
-            $table->boolean("visible")->default(true);
+            $table->string("address",150);
+            $table->string("email",100);
+            $table->string("hostname",150);
+            $table->string("name",100)->unique();
+
+            $table->integer("max_user_count")->default(1);
+
+            $table->string("contact",100)->nullable(true);
+            $table->string("phone",20)->nullable(true);
+            $table->string("url",150)->nullable(true);
+
+            $table->text("favicon")->nullable(true);
+            $table->text("icon")->nullable(true);
+            $table->text("message")->nullable(true);
+
+            $table->json("preferences")->nullable(true);
+
         });
-
-        Schema::table('account', function (Blueprint $table) {
-
-            $table->text("ip")->nullable(true);
-            $table->text("user_agent")->nullable(true);
-
-            $table->timestamps();
-            $table->softDeletes();
-        });
-
+        $this->baseMigration();
+        $this->auditMigration();
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('account');
+    public function down(){
+        Schema::dropIfExists($this->table);
     }
 }
--- database/migrations/2017_07_17_180502_rule_migration.php
+++ database/migrations/2017_07_17_180502_rule_migration.php
@@ -2,54 +2,28 @@
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+use Api\Models\Utils\BaseMigration;
 
-class RuleMigration extends Migration
+
+class RuleMigration extends BaseMigration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('rule', function (Blueprint $table) {
+    protected $table = 'rule';
+    public function up(){
+        Schema::create($this->table, function (Blueprint $table) {
+
+            $table->increments('rule_id');
             $table->integer('account_id')->unsigned();
             $table->foreign('account_id')
                     ->references('account_id')
                     ->on('account')
                     ->onDelete('cascade');
+
         });
-
-        Schema::table('rule', function (Blueprint $table) {
-            $table->increments('rule_id');
-
-            $table->text("detail")->nullable(true);
-            $table->boolean("editable")->default(true);
-            $table->string("code",100)->unique();
-            $table->boolean("enabled")->default(true);
-            $table->text("images")->nullable(true);
-            $table->string("title",100);
-            $table->boolean("visible")->default(true);
-        });
-
-        Schema::table('rule', function (Blueprint $table) {
-
-            $table->text("ip")->nullable(true);
-            $table->text("user_agent")->nullable(true);
-
-            $table->timestamps();
-            $table->softDeletes();
-        });
+        $this->baseMigration();
+        $this->auditMigration();
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('rule');
+    public function down(){
+        Schema::dropIfExists($this->table);
     }
 }
