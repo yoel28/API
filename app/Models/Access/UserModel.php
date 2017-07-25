e53197bb7ca9e90dc711d092814654d1877d15c0
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-25 16:53:43 -0400

    usermodel extends baseModel

--- app/Models/Access/UserModel.php
+++ app/Models/Access/UserModel.php
@@ -2,29 +2,20 @@
 
 namespace Api\Models\Access;
 
+use Api\Models\Utils\BaseModel;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class UserModel extends Authenticatable
+class UserModel extends BaseModel implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
     use Notifiable;
-    protected $table = "users";
+    use Authenticatable, Authorizable, CanResetPassword;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['password', 'remember_token','user_id'];
 }
