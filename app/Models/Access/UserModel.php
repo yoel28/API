09eb874ac419895213dcadbc544a0da79d50454a
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-26 16:41:13 -0400

    add roles & account

--- app/Models/Access/UserModel.php
+++ app/Models/Access/UserModel.php
@@ -16,6 +16,32 @@ class UserModel extends BaseModel implements AuthenticatableContract, Authorizab
     use Notifiable;
     use Authenticatable, Authorizable, CanResetPassword;
 
-    protected $fillable = ['name', 'email', 'password'];
-    protected $hidden = ['password', 'remember_token','user_id'];
+    protected $fillable = ['name', 'email', 'password','account_id','password'];
+    protected $hidden = ['password', 'remember_token','user_id','account_id'];
+
+    protected $appends = ['roles','account'];
+
+    protected function account(){
+        return $this->belongsTo(AccountModel::class,'account_id','account_id');
+    }
+    protected function getAccountAttribute(){
+        return $this->account()->get(['code','title'])[0];
+    }
+
+    protected function roles(){
+        return $this->belongsToMany(
+            RoleModel::class,
+            'user_role',
+            'user_id',
+            'role_id'
+        );
+    }
+    protected function getRolesAttribute(){
+        return $this->roles()->get([])->map(function ($data) {
+            return $data['pivot']['role_id'];
+        });
+    }
+
+
+
 }
