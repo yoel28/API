3135a938f177a926085fa1ced976cd1a110f82a9
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-27 16:34:22 -0400

    owner account domain

---
+++ app/Traits/OwnerAccount.php
@@ -0,0 +1,35 @@
+<?php
+namespace Api\Traits;
+
+use Api\Models\Access\AccountModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+trait OwnerAccount{
+
+    protected $loadAccount = true;
+
+    public function __construct(array $attributes = []){
+        $this->loadAccount();
+        parent::__construct($attributes);
+    }
+
+    protected function loadAccount(){
+        if($this->loadAccount){
+            array_push($this->fillable,'account_id');
+            array_push($this->hidden,'account_id');
+            array_push($this->appends,'account');
+        }
+    }
+
+    protected function account():BelongsTo{
+        return $this->belongsTo(
+            AccountModel::class,
+            'account_id',
+            'id'
+        );
+    }
+    protected function getAccountAttribute(){
+        return $this->account()->get(['code','title'])[0];
+    }
+
+}
