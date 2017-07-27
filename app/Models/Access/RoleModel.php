7fc9d7839ead6f3efd38ad051b7076d288e17191
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-27 15:46:49 -0400

    add permission in role

--- app/Models/Access/RoleModel.php
+++ app/Models/Access/RoleModel.php
@@ -3,16 +3,26 @@
 namespace Api\Models\Access;
 
 use Api\Models\Utils\BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class RoleModel extends BaseModel
 {
 
     protected $appends = ['permissions'];
-    protected $hidden = ['role_id','images'];
+    protected $hidden = ['id','images'];
 
-    //TODO: add associate permissions
     protected function getPermissionsAttribute(){
-
+        return $this->permissions()->get([])->map(function ($data) {
+            return $data['pivot']['permission_id'];
+        });
+    }
+    protected function permissions():BelongsToMany{
+        return $this->belongsToMany(
+            PermissionModel::class,
+            'permission_role',
+            'role_id',
+            'permission_id'
+        );
     }
 
 
