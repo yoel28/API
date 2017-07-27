4eedfe54b8ce52561db960dc5d996f134e36454e
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-27 16:39:29 -0400

    use ownerAccount

--- app/Models/Access/UserModel.php
+++ app/Models/Access/UserModel.php
@@ -3,7 +3,7 @@
 namespace Api\Models\Access;
 
 use Api\Models\Utils\BaseModel;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Api\Traits\OwnerAccount;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\Authenticatable;
@@ -15,23 +15,12 @@ use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class UserModel extends BaseModel implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
-    use Notifiable;
+    use Notifiable,OwnerAccount;
     use Authenticatable, Authorizable, CanResetPassword;
 
-    protected $fillable = ['name', 'email', 'password','account_id'];
-    protected $hidden = ['id','password', 'remember_token','account_id'];
-    protected $appends = ['roles','account'];
-
-    protected function account():BelongsTo{
-        return $this->belongsTo(
-            AccountModel::class,
-            'account_id',
-            'id'
-        );
-    }
-    protected function getAccountAttribute(){
-        return $this->account()->get(['code','title'])[0];
-    }
+    protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['id','password', 'remember_token'];
+    protected $appends = ['roles'];
 
     protected function roles():BelongsToMany{
         return $this->belongsToMany(
