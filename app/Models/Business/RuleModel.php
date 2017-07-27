40cce92049639912679ce7ec15586b48ff68bf22
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-27 16:41:32 -0400

    use owner accout

--- app/Models/Business/RuleModel.php
+++ app/Models/Business/RuleModel.php
@@ -2,21 +2,13 @@
 
 namespace Api\Models\Business;
 
-use Api\Models\Access\AccountModel;
 use Api\Models\Utils\BaseModel;
-
+use Api\Traits\OwnerAccount;
 
 class RuleModel extends BaseModel
 {
-    protected $fillable = ['account_id'];
-    protected $hidden = ['rule_id'];
+    use OwnerAccount;
 
-    public function getAccountIdAttribute($id){
-        return AccountModel::find($id,['code','name']);
-    }
-
-    public function account(){
-        return $this->belongsTo(\Api\Models\Access\AccountModel::class,'account_id','account_id');
-    }
+    protected $hidden = ['id','images'];
 
 }
