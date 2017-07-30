d1d5a97563386111b388d88135dced653439494e
Yoel Rodriguez <yo3l.m18@gmail.com>
2017-07-30 18:51:13 -0400

    use hasPermission

--- app/Models/Access/UserModel.php
+++ app/Models/Access/UserModel.php
@@ -3,8 +3,8 @@
 namespace Api\Models\Access;
 
 use Api\Models\Utils\BaseModel;
+use Api\Traits\HasPermission;
 use Api\Traits\OwnerAccount;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\Passwords\CanResetPassword;
@@ -15,25 +15,11 @@ use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class UserModel extends BaseModel implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
-    use Notifiable,OwnerAccount;
+    use Notifiable,OwnerAccount,HasPermission;
     use Authenticatable, Authorizable, CanResetPassword;
 
     protected $fillable = ['name', 'email', 'password'];
     protected $hidden = ['id','password', 'remember_token'];
-    protected $appends = ['roles'];
-
-    protected function roles():BelongsToMany{
-        return $this->belongsToMany(
-            RoleModel::class,
-            'user_role',
-            'user_id',
-            'role_id'
-        );
-    }
-    protected function getRolesAttribute(){
-        return $this->roles()->get([])->map(function ($data) {
-            return $data['pivot']['role_id'];
-        });
-    }
+    protected $appends = ['roles','permissions'];
 
 }
